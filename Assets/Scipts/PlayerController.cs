ff14dfc67d3cdb86f54774fd53ebc9e29cb4ae7e
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-04-12 22:18:32 +0200

    Updated PlayerController but the rotation not working

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Camera mainCamera; 
     public GameObject Player;
     
     public float speed; 
@@ -16,6 +20,8 @@ public class PlayerController : MonoBehaviour
     
     Transform cameraTransform; 
 
+    private Quaternion targetRotation; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,26 +32,49 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A)){
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if (moveDirection != Vector3.zero)
+        {
+            Player.transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+        }
+
+       /* if(Input.GetKey(KeyCode.A)){
             Debug.Log("A"); 
             rb.AddForce((-Player.transform.right) * speed * Time.deltaTime);
         }
         if(Input.GetKey(KeyCode.W)){
             Debug.Log("B"); 
-            rb.AddForce(Player.transform.forward * speed * Time.deltaTime);
+            rb.AddForce(Player.transform.up * speed * Time.deltaTime);
         } 
         if(Input.GetKey(KeyCode.S)){
             Debug.Log("C"); 
-            rb.AddForce((-Player.transform.forward)* speed * Time.deltaTime);
+            rb.AddForce((-Player.transform.up)* speed * Time.deltaTime);
         } 
         if(Input.GetKey(KeyCode.D)){
             Debug.Log("D"); 
             rb.AddForce(Player.transform.right * speed * Time.deltaTime);
         }
+        */
 
-        if(Input.GetAxis("Mouse X")!= 0){
-            cameraTransform.Rotate(Vector3.up *Input.GetAxis("Mouse X")*mouseSensitive ); 
+         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 mousePosition = ray.GetPoint(rayDistance);
+
+            // Drehe den Spieler zur Mauszeigerposition
+            Vector3 direction = mousePosition - transform.position;
+            direction.y = 0; // Damit die Rotation nur um die Y-Achse erfolgt
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            Player.transform.rotation = lookRotation;
         }
+         
+
         if(Input.GetMouseButtonDown(0)){
             GameObject sphere = Instantiate(SpherePrefab, Player.transform.position+Player.transform.forward, Quaternion.identity);
             Rigidbody sphereRb = sphere.GetComponent<Rigidbody>(); 
